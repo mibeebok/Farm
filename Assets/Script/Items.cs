d87830e4f4a76cdd874e5b01c020fe9fbe3d70cc
mibeebok <averkievavera1@gmail.com>
2025-05-15 01:20:29 +0300

    idk

---
+++ Assets/Script/Items.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    public string nameItem;
+    public int id;
+    public int countItem;
+    public bool isStackable;
+    [Multiline(5)]
+    public string descriptionItem;
+    public string pathPrefabs;
+
+}
